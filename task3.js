f0d56fac7986e60af8ffb2fb6bb3b2c7673b316b
oma16 <48384861+oma16@users.noreply.github.com>
2020-04-18 12:40:52 +0100

    Update task3.js

--- task3.js
+++ task3.js
@@ -7,23 +7,23 @@ function parameter (i) {
     for(i = 1; i <= 100; i++){
       
 if(i % x === 0 && i % y === 0 && i % z === 0){
-        document.write("yu-gi-oh <br>");
+        document.write("yu-gi-oh  ,");
  }else if(i % x === 0 && i % y === 0){
-    document.write("yu-gi <br>");
+    document.write("yu-gi  ,");
 
 }else if(i % x === 0 && i % z === 0){
-    document.write("yu-oh <br>");
+    document.write("yu-oh <br>  ,");
     
 } else if(i % y === 0 && i % z === 0){
-    document.write("gi-oh <br>");
+    document.write("gi-oh  ,");
 } else if(i % x === 0){
-    document.write("yu <br>");
+    document.write("yu  ,");
 }else if(i % y === 0){
-    document.write("gi <br>");
+    document.write("gi  ,");
 }else if(i % z === 0){
-    document.write("oh <br>");
+    document.write("oh  ,");
 }else{
-    document.write(i + ' <br>');
+    document.write(i +  ' ,');
 }
 
     }
